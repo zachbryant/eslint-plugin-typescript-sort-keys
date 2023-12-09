ddf1ca1b04a5202fe0d0a8f46e285f718f246c71
Zach Bryant <zdb282@gmail.com>
2023-12-09 00:40:17 -0800

    remove fuzzing

--- tests/helpers/fuzzer/fuzz.ts
+++
@@ -1,8 +0,0 @@
-import { formatDefinition } from './format'
-import { generateDefinitions } from './generate/generate'
-
-export function fuzz(count: number) {
-  return generateDefinitions(count).map(
-    definition => `export {}; ${formatDefinition(definition)}`,
-  )
-}
